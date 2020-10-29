3b63308e76391cff2566ab7fccba1ee13a8c41e3
Rafiatu <rafibella101@gmail.com>
2020-10-29 16:10:50 +0100

    initial commit

--- project/api/views/auths.py
+++ project/api/views/auths.py
@@ -6,6 +6,9 @@ from rest_framework.permissions import AllowAny
 
 from api.lib.response import Response
 from app.auth.register import Register
+from django.contrib.auth import authenticate
+from rest_framework import status
+# from rest_framework.response import Response
 
 
 class AuthsViewSet(ViewSet):
@@ -13,8 +16,15 @@ class AuthsViewSet(ViewSet):
 
     @action(methods=['post'], detail=False)
     def login(self, request):
-        data = dict(sample='User Login Action')
-        return Response(data)
+        '''Generates an authtoken based on the email and password passed in as parameters.
+        Enter email in the username field e.g: {'username': 'enter_email_here', 'password': 'your_password'}'''
+        username = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(methods=['post'], detail=False)
     def register(self, request):
