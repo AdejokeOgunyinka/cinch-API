17baa36eeea0589191c5d27fe6a263031c35541b
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-11-07 21:44:10 +0100

    bug fix only verified users login

--- project/app/auth/login.py
+++ project/app/auth/login.py
@@ -10,7 +10,12 @@ class Login(Action):
         password = self.data.get("password")
         if email is None or password is None:
             self.fail(dict(invalid_credential = 'Please provide both email and password'))
+        
         user = authenticate(username=email, password=password)
+
+        if not user.email_verified:
+            self.fail(dict(verification_failed='Please verify your account'))
+
         if not user:
             self.fail(dict(invalid_credential='Only registered users can login. Please sign up first.'))
         token, _ = Token.objects.get_or_create(user=user)
