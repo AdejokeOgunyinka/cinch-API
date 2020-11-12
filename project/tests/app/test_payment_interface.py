4c8bac9251a1dd0b98b935cc2b021d4373fa5313
“AdejokeOgunyinka” <ogunyinkajoke@yahoo.com>
2020-11-12 09:28:32 +0100

    feature-payment-interface

--- project/tests/app/test_payment_interface.py
+++ project/tests/app/test_payment_interface.py
@@ -1,5 +1,6 @@
 from payment.payment_interface import PaymentInterface
 from rest_framework.test import APITestCase
+from rest_framework import status
 import json
 
 
@@ -7,24 +8,14 @@ class TestPaymentInterface(APITestCase):
 
     def test_get(self):
         res = PaymentInterface.get('https://api.paystack.co/bank')
-        self.assertIsNotNone(res)
+        self.assertEquals(res.get('status'), True)
 
     def test_get_header(self):
         res = PaymentInterface.get_header(
             'https://api.paystack.co/bank/resolve?account_number=3104841829&bank_code=011')
-        # value = {
-        #             "status": True,
-        #             "message": "Account number resolved",
-        #             "data": {
-        #                     "account_number": "3104841829",
-        #                     "account_name": "OGUNYINKA ADEJOKE ARINOLA",
-        #                     "bank_id": 7
-        #             }
-        # }
-        # self.assertEquals(res, value)
-        self.assertIsNotNone(res)
+        self.assertEquals(res.get('status'), True)
 
     def test_post(self):
         data = json.dumps({"customer": "CUS_d78op1enq5cftcd", "risk_action": "allow"})
         res = PaymentInterface.post('https://api.paystack.co/customer/set_risk_action', data)
-        self.assertIsNotNone(res)
+        self.assertEquals(res.get('status'), None)
