b48c9821c03c7e530f683ffea1cf3f7ade2637fa
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2020-11-12 11:23:17 +0100

    Branch rebase with main

--- project/api/views/__init__.py
+++ project/api/views/__init__.py
@@ -3,5 +3,4 @@ from .auths import AuthsViewSet
 from .artists import ArtistsViewSet
 from .passwords import PasswordsViewSet
 from .location import LocationViewset
-from .bank_list import BankListViewSet
 from .add_account import AccountVerifyViewSet
--- project/payment/bank_verification.py
+++ project/payment/bank_verification.py
@@ -1,6 +1,5 @@
 from .payment_interface import PaymentInterface
 from app.action import Action
-from api.lib.response import Response
 
 
 class BankVerify(Action):
@@ -12,7 +11,7 @@ class BankVerify(Action):
 
         res_url = f'https://api.paystack.co/bank/resolve?account_number={account_number}&bank_code={bank_code}'
 
-        result = PaymentInterface.get_with_header(res_url)
+        result = PaymentInterface.get_with_auth(res_url)
 
         status_message = result.get('status')
 
