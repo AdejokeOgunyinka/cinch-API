cd3a215a774b3477c424c5962d8306ea9e975bb5
Adejoke Ogunyinka <68788948+AdejokeOgunyinka@users.noreply.github.com>
2020-11-23 17:51:46 +0100

    feature seed location (#43)
    * feature seed location
    
    * feature seed location
    
    * feature create location
    
    * feature create location
    
    * feature create location
    
    * feature create location
    
    * feature create location
    
    * feature create location
    
    * unique country_code attribute
    
    * test for incorrect country code

--- project/api/views/location.py
+++ project/api/views/location.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from api.lib.response import Response
 from app.locations.location_action import GetLocation
+from app.locations.create_location import CreateLocation
 
 
 class LocationViewset(ViewSet):
@@ -13,3 +14,12 @@ class LocationViewset(ViewSet):
     def get_location(self, request):
         result = GetLocation.call()
         return Response(result.value, status=status.HTTP_200_OK)
+
+    @action(methods=['post'], detail=False, url_path='create')
+    def create_location(self, request):
+        location = CreateLocation.call(data=request.data)
+
+        if location.failed:
+            return Response(errors=location.error.value, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(location.value, status=status.HTTP_201_CREATED)
